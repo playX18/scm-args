528bef6b6b99de6d91f2313b66d7406c2df52d76
playX18 <adel.prokurov281@outlook.com>
2026-06-18 11:57:58 +0700

    update spkg manifest

--- spkg.scm
+++ spkg.scm
@@ -1,6 +1,24 @@
-(package 
+(package
   (name (args))
   (version "0.1.1")
-  (libraries (args)))
+  (license "BSD-3-Clause")
+  (description "R7RS-small CLI parser library")
+  (authors "Adel Prokurov")
+  (readme "README.md")
+  (dialects r7rs)
+  (source-path "src")
+  (libraries
+    ((r7rs (args))
+     (r7rs (args grammar))
+     (r7rs (args help optional))
+     (r7rs (args option))
+     (r7rs (args parser))
+     (r7rs (args results))
+     (r7rs (args runner))
+     (r7rs (args string)))))
 
 (dependencies)
+
+(dev-dependencies)
+
+(overrides)
